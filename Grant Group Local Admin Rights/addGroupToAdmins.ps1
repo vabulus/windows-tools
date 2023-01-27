1ec664c89a01a52c4b21d97e2503aaf613a2f114
Fabulus <fabio@kreisern.eu>
2023-01-27 13:24:45 +0100

    grant group admin rights

---
+++ Grant Group Local Admin Rights/addGroupToAdmins.ps1
@@ -0,0 +1,9 @@
+$group_to_add = "ms1fk\24-Admins"
+
+$permissions = "SeAuditPrivilege", "SeSecurityPrivilege", "SeBackupPrivilege", "SeCreateSymbolicLinkPrivilege", "SeMachineAccountPrivilege", "SeShutdownPrivilege", "SeRemoteShutdownPrivilege", "SeCreatePagefilePrivilege", "SeIncreaseQuotaPrivilege", "SeRestorePrivilege  "", "SeSystemTimePrivilege", "SeTimeZonePrivilege", "SeTakeOwnershipPrivilege", "SeEnableDelegationPrivilege", "SeUndockPrivilege",
+
+
+foreach ($permission in $permissions){
+    .\ntrights.exe +r $permission -u $group_to_add
+}
+
